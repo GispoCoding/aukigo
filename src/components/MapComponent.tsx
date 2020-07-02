fd70d5217d58f6e97dbf2f200045334976986778
Mikael Vaaltola <mvaaltola@gmail.com>
2020-07-02 11:48:07 +0300

    Create initial mapcomponent

---
+++ src/components/MapComponent.tsx
@@ -0,0 +1,34 @@
+import React, { useEffect } from 'react';
+import 'ol/ol.css';
+import { Map, View } from 'ol';
+import TileLayer from 'ol/layer/Tile';
+import OSM from 'ol/source/OSM';
+
+const MapComponent = () => {
+  const mapContainerStyle = { height: '100%', width: '100%' };
+
+  const map = new Map({ // eslint-disable-line no-new
+    target: undefined,
+    layers: [
+      new TileLayer({
+        source: new OSM(),
+      }),
+    ],
+    view: new View({
+      center: [0, 0],
+      zoom: 0,
+    }),
+  });
+
+  useEffect(() => {
+    map.setTarget('map');
+  }, [map]);
+
+  return (
+    <div style={mapContainerStyle}>
+      <div id="map" style={mapContainerStyle} />
+    </div>
+  );
+};
+
+export default MapComponent;
