4a656df11f150c2ccc66e03f6b2b9a910bb4fffe
Mikael Vaaltola <mvaaltola@gmail.com>
2020-07-17 13:30:05 +0300

    Add placeholder theme module

---
+++ src/theme.ts
@@ -0,0 +1,34 @@
+import { createMuiTheme } from '@material-ui/core';
+import { orange, blue } from '@material-ui/core/colors';
+
+declare module '@material-ui/core/styles/createMuiTheme' {
+  interface Theme {
+    custom: {
+      variable: string,
+    },
+  }
+  interface ThemeOptions {
+    custom?: {
+      variable: string,
+    }
+  }
+}
+
+const defaultTheme = createMuiTheme({
+  overrides: {
+    MuiGrid: {
+      item: {
+        pointerEvents: 'auto',
+      },
+    },
+  },
+  palette: {
+    primary: blue,
+    secondary: orange,
+  },
+  custom: {
+    variable: '#000',
+  },
+});
+
+export default defaultTheme;
