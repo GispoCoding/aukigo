f735722af3355f4a0abeebbb8fdf80a1ba79ee45
Mikael Vaaltola <mvaaltola@gmail.com>
2020-07-30 17:59:55 +0300

    Add search and settings to header bar

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,53 +1,114 @@
 import React from 'react';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import IconButton from '@material-ui/core/IconButton';
+import Typography from '@material-ui/core/Typography';
+import InputBase from '@material-ui/core/InputBase';
 import {
-  AppBar, Toolbar, IconButton, Typography, Button,
-  makeStyles, createStyles, Grid,
-} from '@material-ui/core';
+  createStyles, fade, Theme, makeStyles,
+} from '@material-ui/core/styles';
+import { Search, Settings } from '@material-ui/icons';
+import { Grid } from '@material-ui/core';
 
-const useStyles = makeStyles((theme) => (
-  createStyles({
-    root: {
-      margin: '30px auto 0 auto',
-      flexGrow: 1,
-      pointerEvents: 'auto',
+const useStyles = makeStyles((theme: Theme) => createStyles({
+  root: {
+    backgroundColor: theme.palette.primary.main,
+    flexGrow: 1,
+    margin: '30px auto 0 auto',
+    pointerEvents: 'auto',
+    [theme.breakpoints.up('md')]: {
+      margin: '30px 30px 0 auto',
     },
-    mainBar: {
-      color: theme.palette.primary.main,
-      backgroundColor: theme.palette.secondary.main,
+    [theme.breakpoints.down('sm')]: {
+      paddingRight: 0,
     },
-    logo: {
-      marginRight: '20px',
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    marginLeft: '16px',
+    flexGrow: 1,
+    pointerEvents: 'none',
+    userSelect: 'none',
+    display: 'none',
+    [theme.breakpoints.up('sm')]: {
+      display: 'block',
     },
-    descTxt: {
-      flexGrow: 1,
+  },
+  search: {
+    position: 'relative',
+    borderRadius: theme.shape.borderRadius,
+    backgroundColor: fade(theme.palette.common.white, 0.15),
+    '&:hover': {
+      backgroundColor: fade(theme.palette.common.white, 0.25),
     },
-  })
-));
+    margin: '8px 0 8px 8px',
+    width: '100%',
+    [theme.breakpoints.up('sm')]: {
+      marginLeft: theme.spacing(1),
+      width: 'auto',
+    },
+  },
+  searchIcon: {
+    padding: theme.spacing(0, 2),
+    height: '100%',
+    position: 'absolute',
+    pointerEvents: 'none',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  inputRoot: {
+    color: 'inherit',
+  },
+  inputInput: {
+    padding: theme.spacing(2, 1, 2, 0),
+    // vertical padding + font size from searchIcon
+    paddingLeft: `calc(1em + ${theme.spacing(4)}px)`,
+    transition: theme.transitions.create('width'),
+    width: '100%',
+    [theme.breakpoints.up('sm')]: {
+      width: '12ch',
+      '&:focus': {
+        width: '20ch',
+      },
+    },
+  },
+}));
 
 export default function Header() {
   const classes = useStyles();
+
   return (
     <Grid
       className={classes.root}
-      direction="row"
       item
+      md={6}
       sm={11}
       xs={12}
     >
-      <AppBar
-        className={classes.mainBar}
-        position="static"
-      >
-        <Toolbar>
-          <IconButton className={classes.logo}>
+      <AppBar position="static">
+        <Toolbar disableGutters>
+          <Typography className={classes.title} variant="h6" noWrap>
             aukigo
-          </IconButton>
-          <Typography className={classes.descTxt}>
-            Some text
           </Typography>
-          <Button>
-            Button
-          </Button>
+          <div className={classes.search}>
+            <div className={classes.searchIcon}>
+              <Search />
+            </div>
+            <InputBase
+              placeholder="Search…"
+              classes={{
+                root: classes.inputRoot,
+                input: classes.inputInput,
+              }}
+              inputProps={{ 'aria-label': 'search' }}
+            />
+          </div>
+          <IconButton color="inherit">
+            <Settings />
+          </IconButton>
         </Toolbar>
       </AppBar>
     </Grid>
