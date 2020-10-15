39327f2a7beea56ee8ddc7c51d98f7a2ccae1af8
anniinakovalainen <anniina.kovalainen@gmail.com>
2020-10-15 16:37:14 +0300

    Add shadow effects to pop ups

--- src/components/mappopup.tsx
+++ src/components/mappopup.tsx
@@ -24,6 +24,7 @@ const useStyles = makeStyles((theme: Theme) => (
       color: theme.palette.primary.dark,
       padding: '10px 10px 10px 20px',
       margin: '5px',
+      boxShadow: '0.08em 0.2em 0.5em 0em rgba(9,38,47,0.2), 0em 0em 0em 0.01em rgba(9,38,47,0.1), 0.1em 0.2em 0.5em 0em rgba(9,38,47,0.2)',
       '&&& td': {
         paddingRight: '10px',
       },
